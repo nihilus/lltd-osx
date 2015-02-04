cab5975669ca61c0d6500b8c8631afb32ff2a168
Markus Gothe <nietzsche@lysator.liu.se>
2015-02-04 03:14:36 +0100

    Add more functionality.

--- src/bpflib.c
+++ src/bpflib.c
@@ -37,21 +37,79 @@
 
 #include "bpflib.h"
 
-#ifdef TESTING
-#include "util.h"
-#endif /* TESTING */
+#define BPF_FORMAT	"/dev/bpf%d"
 
+/*
+ bpf_set_timeout - Sets the read timeout parameter. The timeval specifies the length
+ of time to wait before timing out a read request. This paramter is init'd to 0 by 
+ open(2) indicating no timeout
+ */
 int
 bpf_set_timeout(int fd, struct timeval * tv_p)
 {
     return (ioctl(fd, BIOCSRTIMEOUT, tv_p));
 }
 
+/*
+ bpf_get_data_link - Returns the type of the data link layer underlying the attached 
+ interface.  EINVAL is returned if no inter-face has been specified.  The device types, 
+ prefixed with ``DLT_'', are defined in <net/bpf.h>.
+ 
+ */
+int
+bpf_get_data_link(int fd, u_int * dl_p)
+{
+    return (ioctl(fd, BIOCGDLT, dl_p));
+}
+
+/*
+ bpf_get_blen - returns the required buffer length for reads on bpf files
+ */
 int 
 bpf_get_blen(int fd, int * blen)
 {
     return(ioctl(fd, BIOCGBLEN, blen));
 }
+
+/*
+ bpf_set_promiscuous - Forces the interface into promiscuous mode.  All packets,
+ not just those destined for the local host, are processed. Since more than one file 
+ can be listening on a given interface, a listener that opened its interface non-
+ promiscuously may receive packets promiscuously.  This problem can be remedied with 
+ an appropriate filter.
+ */
+int
+bpf_set_promiscuous(int bpf_fd, int offon) /* NB: int offon = 1 will disable and int offon = 0 will enable. */
+{
+    return(ioctl(bpf_fd, BIOCPROMISC, offon));    
+} 
+
+/*
+ bpf_get_stats - (struct bpf_stat) Returns the following structure of
+ packet statistics:
+ 
+ struct bpf_stat {
+	 u_int bs_recv;     // number of packets received
+	 u_int bs_drop;     // number of packets dropped 
+ };
+ 
+ The fields are:
+ 
+ bs_recv the number of packets received by the
+ descriptor since opened or reset (including
+								   any buffered since the last read call); and
+ 
+ bs_drop the number of packets which were accepted by
+ the filter but dropped by the kernel because
+ of buffer overflows (i.e., the application's
+					  reads aren't keeping up with the packet
+					  traffic).
+ */
+int
+bpf_get_stats(int bpf_fd, struct bpf_stat *bs_p)
+{
+    return ioctl(bpf_fd, BIOCGSTATS, bs_p);  
+} 
 
 int
 bpf_dispose(int bpf_fd)
@@ -69,7 +127,7 @@ bpf_new()
     int fd = -1;
 
     for (i = 0; true; i++) {
-	snprintf(bpfdev, sizeof(bpfdev), "/dev/bpf%d", i);
+	snprintf(bpfdev, sizeof(bpfdev), BPF_FORMAT, i);
 	fd = open(bpfdev, O_RDWR , 0);
 	if (fd >= 0) {
 #ifdef SO_TC_CTL
@@ -85,6 +143,11 @@ bpf_new()
     return (fd);
 }
 
+/*
+ bpf_setif - Sets the hardware interface associated with the file descriptor. this call
+ must be made before any packets can be read. the device name is indicated by setting the
+ ifr_name field of the ifreq. Also performs the actions of BIOCFLUSH
+ */
 int
 bpf_setif(int fd, const char * en_name)
 {
@@ -94,6 +157,13 @@ bpf_setif(int fd, const char * en_name)
     return (ioctl(fd, BIOCSETIF, &ifr));
 }
 
+/*
+ bpf_set_immediate - enable or disable immediate mode based on the truth value of argument
+ value 0 - disable, not 0 - enable.
+ When immediate mode is enabled, reads return immediately upon packet reception. Otherwise
+ a read will block until either the kernel buffer becomes full or a timeout occurs. Default
+ setting is off.
+ */
 int
 bpf_set_immediate(int fd, u_int value)
 {
@@ -129,68 +199,12 @@ bpf_arp_filter(int fd, int type_offset, int type, int pkt_size)
     return ioctl(fd, BIOCSETF, &prog);
 }
 
+int bpf_see_sent(int fd, int value) {
+  return(ioctl(fd, BIOCSSEESENT, &value));
+}
+
 int
 bpf_write(int fd, void * pkt, int len)
 {
     return (write(fd, pkt, len));
 }
-
-#ifdef TESTING
-#include <net/if_arp.h>
-#include <net/ethernet.h>
-#include <netinet/if_ether.h>
-
-
-void
-bpf_read_continuously(int fd, u_int blen)
-{
-    int n;
-    char * rxbuf = malloc(blen);
-
-    printf("rx buf len is %d\n", blen);
-    while (1) {
-	n = read(fd, rxbuf, blen);
-	if (n < 0) {
-	    perror("bpf_read_continuously");
-	    return;
-	}
-	if (n == 0)
-	    continue;
-	print_data(rxbuf, n);
-    }
-}
-
-int
-main(int argc, char * argv[])
-{
-    int fd = bpf_new();
-    char * en_name = "en0";
-    u_int bpf_blen = 0;
-
-    if (fd < 0) {
-	perror("no bpf devices");
-	exit(1);
-    }
-    
-    if (argc > 1)
-	en_name = argv[1];
-    (void)bpf_set_immediate(fd, 1);
-    if (bpf_arp_filter(fd, 12, ETHERTYPE_ARP, 
-		       sizeof(struct ether_arp) + sizeof(struct ether_header)) 
-	< 0) {
-	perror("bpf_arp_filter");
-    }
-    if (bpf_setif(fd, en_name) < 0) {
-	perror("bpf_attach");
-	exit(1);
-    }
-
-    if (bpf_get_blen(fd, &bpf_blen) < 0) {
-	perror("bpf_get_blen");
-	exit(1);
-    }
-    bpf_read_continuously(fd, bpf_blen);
-    exit(0);
-    return (0);
-}
-#endif /* TESTING */
--- src/bpflib.h
+++ src/bpflib.h
@@ -26,14 +26,17 @@
 #define _S_BPFLIB_H
 #include <net/bpf.h>
 
+int bpf_set_timeout(int fd, struct timeval * tv_p);
+int bpf_get_data_link(int fd, u_int * dl_p);
 int bpf_get_blen(int fd, int * blen);
+int bpf_set_promiscuous(int bpf_fd, int offon);
+int bpf_get_stats(int bpf_fd, struct bpf_stat *bs_p);
+int bpf_dispose(int bpf_fd);
 int bpf_new();
-int bpf_dispose(int fd);
 int bpf_setif(int fd, const char * en_name);
 int bpf_set_immediate(int fd, u_int value);
 int bpf_filter_receive_none(int fd);
-int bpf_arp_filter(int fd, int type_offset, int type, int packet_size);
-int bpf_set_timeout(int fd, struct timeval * tv_p);
+int bpf_see_sent(int fd, int value);
 int bpf_write(int fd, void * pkt, int len);
 
 
